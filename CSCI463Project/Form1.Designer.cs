e80e277b6087f51cafeee119067cb96849e59d7c
GrobMonster <wyatt.brusseau@gmail.com>
2026-04-26 23:26:34 -0500

    Fixed Login page

--- CSCI463Project/Form1.Designer.cs
+++ CSCI463Project/Form1.Designer.cs
@@ -33,15 +33,16 @@
             PasswordText = new TextBox();
             label1 = new Label();
             LoginPanel = new Panel();
-            LoginPanel.SuspendLayout();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // LoginButton
             // 
-            LoginButton.Location = new Point(68, 240);
-            LoginButton.Margin = new Padding(2, 2, 2, 2);
+            LoginButton.Location = new Point(93, 318);
+            LoginButton.Margin = new Padding(2);
             LoginButton.Name = "LoginButton";
-            LoginButton.Size = new Size(99, 38);
+            LoginButton.Size = new Size(124, 48);
             LoginButton.TabIndex = 0;
             LoginButton.Text = "LOGIN";
             LoginButton.UseVisualStyleBackColor = true;
@@ -49,56 +50,67 @@
             // 
             // UserIdText
             // 
-            UserIdText.ForeColor = Color.Gray;
-            UserIdText.Location = new Point(44, 165);
+            UserIdText.ForeColor = Color.Black;
+            UserIdText.Location = new Point(29, 203);
+            UserIdText.Margin = new Padding(4);
             UserIdText.Name = "UserIdText";
-            UserIdText.Size = new Size(206, 31);
+            UserIdText.PlaceholderText = "UserID";
+            UserIdText.Size = new Size(256, 31);
             UserIdText.TabIndex = 2;
             // 
             // PasswordText
             // 
-            PasswordText.ForeColor = Color.Gray;
-            PasswordText.Location = new Point(44, 222);
+            PasswordText.ForeColor = Color.Black;
+            PasswordText.Location = new Point(29, 263);
+            PasswordText.Margin = new Padding(4);
             PasswordText.Name = "PasswordText";
-            PasswordText.Size = new Size(206, 31);
+            PasswordText.PlaceholderText = "Password";
+            PasswordText.Size = new Size(256, 31);
             PasswordText.TabIndex = 3;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 26F);
-            label1.Location = new Point(31, 42);
+            label1.Location = new Point(39, 52);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(231, 70);
+            label1.Size = new Size(246, 70);
             label1.TabIndex = 4;
             label1.Text = "ClearPath";
             // 
             // LoginPanel
             // 
             LoginPanel.BackColor = Color.DarkGray;
-            LoginPanel.Controls.Add(label1);
-            LoginPanel.Controls.Add(PasswordText);
-            LoginPanel.Controls.Add(UserIdText);
-            LoginPanel.Controls.Add(LoginButton);
             LoginPanel.Location = new Point(0, 1);
             LoginPanel.Name = "LoginPanel";
             LoginPanel.Size = new Size(310, 449);
             LoginPanel.TabIndex = 5;
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Gray;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(PasswordText);
+            panel1.Controls.Add(UserIdText);
+            panel1.Controls.Add(LoginButton);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(336, 564);
+            panel1.TabIndex = 5;
+            // 
             // LoginPage
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(255, 192, 255);
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
-            Controls.Add(PasswordText);
-            Controls.Add(UserIdText);
-            Controls.Add(LoginButton);
+            BackColor = Color.White;
+            ClientSize = new Size(1000, 562);
+            Controls.Add(panel1);
+            Margin = new Padding(4);
             Name = "LoginPage";
             Text = "Login Page";
-            LoginPanel.ResumeLayout(false);
-            LoginPanel.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -109,5 +121,6 @@
         private TextBox PasswordText;
         private Label label1;
         private Panel LoginPanel;
+        private Panel panel1;
     }
 }
